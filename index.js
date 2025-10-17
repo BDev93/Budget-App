d2cdfc041410f0740816def9d6c276850e9ddbb5
Bartek Zaborowski <b.zaborowski.bz@gmail.com>
2025-10-17 19:07:01 +0200

    Adding input support

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 
 const addBtn = document.getElementById("add-btn")
+const inputAdd = document.getElementById("input-add")
+const revenuesDiv = document.getElementById("revenues")
+const expensesDiv = document.getElementById("expenses")
 
 addBtn.addEventListener('click', function(){
-    console.log('hejka')
+    const valueStr = inputAdd.value.trim();
+    if(valueStr === ""){
+        alert("Wpisz kwotę.")
+        return
+    }
+
+    const amount = parseFloat(valueStr.replace(',', '.'))
+    if(isNaN(amount)){
+        alert('Podaj poprawną liczbę (np. 123.45')
+        return
+    }
+
+    const selectRadio = document.querySelector('input[name="type"]:checked')
+    if(!selectRadio) {
+        alert("Wybierz Przychody lub Wydatki.")
+        return
+    }
+
 })
